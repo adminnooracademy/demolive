adcc5942b0a2232955e9a928a43d46f4ef305285
adminnooracademy <admin.nooracademy@gmail.com>
2026-05-24 10:20:09 +0700

    config.js

--- config.js
+++ config.js
@@ -8,13 +8,12 @@ window.PMB_CONFIG = {
   // WAJIB DIISI agar admin bisa pantau realtime.
   // Ambil dari Firebase Console > Project settings > Your apps > Web app.
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+   apiKey: "AIzaSyDOBfe4INgVtimBkwVgfQSHT8nkOS-RL0M",
+   authDomain: "demolive-45286.firebaseapp.com",
+   projectId: "demolive-45286",
+   storageBucket: "demolive-45286.firebasestorage.app",
+   messagingSenderId: "809618354310",
+   appId: "1:809618354310:web:c3ef1ec39ce90320566107",
   },
 
   // Biarkan true. Aktifkan Anonymous di Firebase Authentication.
